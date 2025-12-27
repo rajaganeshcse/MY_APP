a4a60fba6d84dedd9ba97d543a895813f01a4e97
Ganesh <ganesh209832@gmail.com>
2025-12-28 00:01:41 +0530

    c

--- app/src/main/java/com/example/rgamer/activity_daily_spin.java
+++ app/src/main/java/com/example/rgamer/activity_daily_spin.java
@@ -58,7 +58,7 @@ public class activity_daily_spin extends AppCompatActivity {
         setContentView(R.layout.activity_daily_spin);
         makeFullScreen();
 
-        enableFullScreen(); // 🔥 FULL SCREEN
+        // 🔥 FULL SCREEN
 
         userPref = new UserPref(this);
         db = FirebaseFirestore.getInstance();
@@ -114,35 +114,12 @@ public class activity_daily_spin extends AppCompatActivity {
        FULL SCREEN MODE
        ================================================== */
 
-    private void enableFullScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowInsetsController controller =
-                    getWindow().getInsetsController();
-            if (controller != null) {
-                controller.hide(
-                        WindowInsets.Type.statusBars()
-                                | WindowInsets.Type.navigationBars()
-                );
-                controller.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                );
-            }
-        } else {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            );
-        }
-    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        enableFullScreen(); // 🔁 re-apply
+
     }
 
     /* ==================================================
