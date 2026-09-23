e6d5c3ee157160fe21827cc81d7bb88ee2949732
ganesh <ganesh209832@gmail.com>
2026-09-23 11:32:46 +0530

    Update minSdk to 24 for Google Play automatic protection compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,12 @@ android {
     defaultConfig {
         applicationId = "com.dailykash.app"
 
-        minSdk = 23
+        minSdk = 24
 
         targetSdk = 35
 
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
